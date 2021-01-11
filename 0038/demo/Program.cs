fe5b7b78c1000547ed492b2c67e1d05a8d90262c
humornif <humornif@hotmail.com>
2021-01-11 16:35:54 +0800

    0038 Init

---
+++ 0038/demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace demo
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .Build()
+            .Run();
+    }
+}
+}
---
+++ 0038/demo/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace demo
+{
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddControllers();
+        services.AddRazorPages();
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoint =>
+        {
+            endpoint.MapDefaultControllerRoute();
+            endpoint.MapControllers();
+            endpoint.MapRazorPages();
+            endpoint.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello from Demo");
+            });
+            endpoint.MapGet("/test", async context =>
+            {
+                await context.Response.WriteAsync("Hello from Demo.Test");
+            });
+
+            endpoint.MapGet("/about", async context =>
+            {
+                await context.Response.WriteAsync("Hello from Demo.About");
+            });
+        });
+    }
+}
+}

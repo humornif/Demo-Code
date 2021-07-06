67844297f11bfb02ad53cb71b31d4af8fc3d01b3
humornif <humornif@hotmail.com>
2021-07-06 18:55:21 +0800

    init 0053

---
+++ 0053/demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommandLineTool;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cli cli = new Cli(typeof(TestCLI))
+            {
+                Introduction = "这是一个 Demo 应用",
+                PromptText = "WangPlus",
+            };
+
+            cli.SetCancellationKeys(new() { "exit" });
+            cli.Start();
+        }
+    }
+}
---
+++ 0053/demo/TestCLI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLineTool.Attributes;
+
+namespace demo
+{
+    [App("Demo")]
+    public class TestCLI
+    {
+        [Command("hello", "就是打个招呼")]
+        public static void Hello(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+        [Command("multiinput", "多个参数")]
+        public static void MultiInput([ParamArgument()] List<string> names)
+        {
+            foreach (var item in names)
+            {
+                Console.WriteLine($"Hello {item}");
+            }
+        }
+        [Command("multifile", "多个文件")]
+        public static void MultiFile([ParamArgument()] List<FileInfo> files)
+        {
+            foreach (var item in files)
+            {
+            }
+        }
+        [Command("withpara", "额外参数")]
+        public static void WithPara([ParamArgument()] string names, [ParamOption("-a")] string op1)
+        {
+        }
+    }
+}
